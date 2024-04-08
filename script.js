274736a1bd64bbb01a41a831465cc45d555ce681
gitdishant <92519667+gitdishant@users.noreply.github.com>
2024-04-08 08:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// your code here
-const name = document.querySelector("#name").value;
-const year = document.querySelector("#year").value;
-
-const hed = document.querySelector("#url").innerHTML += name + year
+function changeHeading(){
+  var inputElement1 = document.getElementById("name").value;
+  var inputElement2 = document.getElementById("year").value;
+  var changeUrl = document.getElementById("url")
+  
+  var innerHtml = `?name=${inputElement1}&year=${inputElement2} ` 
+  
+  changeUrl.innerHTML += innerHtml ;
+  
+}
