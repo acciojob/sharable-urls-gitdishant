79ccd62f87b4da2e227f6678bd5ca047874387ce
gitdishant <92519667+gitdishant@users.noreply.github.com>
2024-04-08 07:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // your code here
+const name = document.querySelector("#name").value;
+const year = document.querySelector("#year").value;
+
+const hed = document.querySelector("#url").innerHTML += name + year
